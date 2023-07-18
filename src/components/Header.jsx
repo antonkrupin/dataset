319748c42fc850f30a72dfc832fa7db9edeab12e
antonkrupin <krupin_anton@mail.ru>
2023-07-18 14:54:21 +0300

    change header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 
 const Header = () => {
+	const location = useLocation();
+	
 	return (
 		<div className="d-flex justify-content-center mb-5 mt-5">
-			<Button className='m-1' variant="success">Create new dataset</Button>
-			<Button className='m-1' variant="danger">Clear dataset</Button>
+			<Button className='m-1' variant="success">Add new dataset</Button>
+			<Button link="/addDataset" className='m-1' variant="danger">Clear dataset</Button>
+			{location.pathname === '/addDataset' && (
+				<Link to="/">Back</Link>
+			)}
+			{
+				location.pathname !== '/addDataset' && (
+					<Link to="/addDataset">Add new dataset</Link>
+				)
+			}
 		</div>
 	)
 }
