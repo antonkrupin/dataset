7ef9b09eb89214939c75757dc36f8b60d181e72e
antonkrupin <krupin_anton@mail.ru>
2023-07-20 13:17:05 +0300

    delete unused components

--- src/components/Intersection.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Intersection = () => {
-	return (
-		<h1>Intersection</h1>
-	)
-}
-
-export default Intersection;
--- src/components/Visualisation.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Visualisation = () => {
-	return (
-		<h1>Visualisation</h1>
-	)
-}
-
-export default Visualisation;
