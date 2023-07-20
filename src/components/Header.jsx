a8fd51e547f0dc46393f80113ae0710e802740cc
antonkrupin <krupin_anton@mail.ru>
2023-07-20 13:18:04 +0300

    delete buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
+
 
 const Header = () => {
 	const location = useLocation();
 	
 	return (
 		<div className="d-flex justify-content-center mb-5 mt-5">
-			<Button className='m-1' variant="success">Add new dataset</Button>
-			<Button link="/addDataset" className='m-1' variant="danger">Clear dataset</Button>
 			{location.pathname === '/addDataset' && (
 				<Link to="/">Back</Link>
 			)}
