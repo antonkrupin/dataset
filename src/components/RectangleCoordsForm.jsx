74c58edae9e202553711ee53c7d5c0d2d3f3970f
Anton Krupin <krupin_anton@mail.ru>
2023-07-20 19:27:49 +0300

    delete comments

--- src/components/RectangleCoordsForm.jsx
+++ src/components/RectangleCoordsForm.jsx
@@ -1,6 +1,10 @@
 import React, { useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchRectangleCoords, fetchDataSet, fetchError } from '../slices/selectors';
+import {
+	fetchRectangleCoords,
+	fetchDataSet,
+	fetchError
+} from '../slices/selectors';
 
 import { Button } from 'react-bootstrap';
 
@@ -11,34 +15,6 @@ import {
 	setError,
 } from '../slices/mainReducer';
 
-/*const test = (point, coordinates) => {
-	if ((coordinates[0]*10 >= rectCoord[0]*10 && coordinates[0]*10 <= rectCoord[2]*10) && (coordinates[1]*10 >= rectCoord[1]*10 && coordinates[1]*10 <= rectCoord[3]*10)) {
-		return point;
-	}
-}*/
-
-/*const checkedPoints = points.filter((point) => {
-	const { coordinates } = point;
-	console.log('coordinates', coordinates);
-	console.log('rectcoord', rectCoord);
-	if ((coordinates[0]*10 >= rectCoord[0]*10 && coordinates[0]*10 <= rectCoord[2]*10) && (coordinates[1]*10 >= rectCoord[1]*10 && coordinates[1]*10 <= rectCoord[3]*10)) {
-		return point;
-	}
-});*/
-
-/*const checkPoints = (points, coordinates, rectCoord) => {
-	const x = coordinates[0];
-	const y = coordinates[1];
-	const x1 = rectCoord[0];
-	const y1 = rectCoord[2];
-	const x2 = rectCoord[1];
-	const y2 = rectCoord[3];
-	// a - x2 - x1
-	//b y2 - y1
-	console.log(Math.abs(x2 - x1));
-	console.log(Math.abs(y2 - y1));
-}*/
-
 const RectangleCoordsForm = () => {
 	const dispatch = useDispatch();
 	const formRef = useRef();
@@ -46,32 +22,25 @@ const RectangleCoordsForm = () => {
 	const points = useSelector(fetchDataSet);
 	const error = useSelector(fetchError);
 	
-	const checkPoints = (e) => {
+	const getMatchedDatasets = (e) => {
 		e.preventDefault();
 		const formData = new FormData(formRef.current);
-		const rectCoord = [formData.get('x1'),formData.get('y1'),formData.get('x2'),formData.get('y2')].map((elem) => parseInt(elem));
-
-		if (rectCoord[0] === rectCoord[2] || rectCoord[1] === rectCoord[3]) {
+		const coordX = [parseInt(formData.get('x1')), parseInt(formData.get('x2'))].sort();
+		const coordY = [parseInt(formData.get('y1')), parseInt(formData.get('y2'))].sort();
+		
+		if (coordX[0] === coordX[1] || coordY[0] === coordY[1]) {
 			dispatch(setError('Check coordinates. Can`t build rectangle.'));
 		} else {
 			const checkedPoints = points.filter((point) => {
 				const { coordinates } = point;
-				const x1 = rectCoord[0];
-				const y1 = rectCoord[1];
-				const x2 = rectCoord[2];
-				const y2 = rectCoord[3];
 				
-				console.log(Math.abs(coordinates[0]) / (x2 - x1));
-				console.log(Math.abs(coordinates[1]) / (y2 - y1));
-				//d = (x3 − x2) ∙ (y1 − y2) − (x1 − x2) ∙ (y3 − y2)
-				//0 ≤ (y1 − y2) ∙ (x − x2) − (x1 − x2) ∙ (y − y2) ≤ d
-				//0 ≤ (x3 − x2) ∙ (y − y2) − (y3 − y2) ∙ (x − x2) ≤ d
-				if ((coordinates[0]*10 >= rectCoord[0]*10 && coordinates[0]*10 <= rectCoord[2]*10) && (coordinates[1]*10 >= rectCoord[1]*10 && coordinates[1]*10 <= rectCoord[3]*10)) {
+				if ((coordinates[0]*10 >= coordX[0]*10 && coordinates[0]*10 <= coordX[1]*10) &&
+						(coordinates[1]*10 >= coordY[0]*10 && coordinates[1]*10 <= coordY[1]*10)) {
 					return point;
 				}
 			});
 	
-			dispatch(setRectangleCoords(rectCoord));
+			dispatch(setRectangleCoords([coordX[0], coordY[0], coordX[1], coordY[1]]));
 			dispatch(setIsRectangleCoordsLoaded());
 			dispatch(setError());
 			if (checkedPoints.length !== 0) {
@@ -88,18 +57,52 @@ const RectangleCoordsForm = () => {
 		dispatch(setMatchedDatasets(null));
 		formRef.current.reset();
 	}
+
 	return (
 		<>
 			<h4>Enter rectangle coordinates to check points</h4>
-			<form onSubmit={checkPoints} type="submit" className="d-flex justify-content-around rectangleForm" ref={formRef}>
-				<input id="x1" type="text" placeholder="x1" name="x1" required disabled={rectangleCoords ? true : false}/>
-				<input id="y1" type="text" placeholder="y1" name="y1" required disabled={rectangleCoords ? true : false}/>
-				<input id="x2" type="text" placeholder="x2" name="x2" required disabled={rectangleCoords ? true : false}/>
-				<input id="y2" type="text" placeholder="y2" name="y2" required disabled={rectangleCoords ? true : false}/>
+			<form
+				onSubmit={getMatchedDatasets}
+				type="submit"
+				className="d-flex justify-content-around rectangleForm"
+				ref={formRef}
+			>
+				<input
+					id="x1"
+					type="text"
+					placeholder="x1"
+					name="x1"
+					required
+					disabled={rectangleCoords ? true : false
+				}/>
+				<input
+					id="y1"
+					type="text"
+					placeholder="y1"
+					name="y1"
+					required
+					disabled={rectangleCoords ? true : false}
+				/>
+				<input
+					id="x2"
+					type="text"
+					placeholder="x2"
+					name="x2"
+					required
+					disabled={rectangleCoords ? true : false}
+				/>
+				<input
+					id="y2"
+					type="text"
+					placeholder="y2"
+					name="y2"
+					required
+					disabled={rectangleCoords ? true : false}
+				/>
 				<Button type="submit" disabled={rectangleCoords ? true : false}>Check</Button>
 				<Button onClick={resetForm} className="btn btn-danger" disabled={rectangleCoords ? false : true}>Clear</Button>
 			</form>
-			<div>{error}</div>
+			<div className="text-danger">{error}</div>
 		</>
 	)
 };
