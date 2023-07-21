ca0e2966eec1b8b51f481f78fb35fd395ea04e71
antonkrupin <krupin_anton@mail.ru>
2023-07-21 08:49:01 +0300

    add isLoading check

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
+
+import { fetchIsLoading } from '../slices/selectors';
 
 
 const Header = () => {
 	const location = useLocation();
+	const isLoading = useSelector(fetchIsLoading);
 	
 	return (
-		<div className="d-flex justify-content-center mb-5 mt-5">
-			{location.pathname === '/addDataset' && (
-				<Link to="/">Back</Link>
-			)}
-			{
-				location.pathname !== '/addDataset' && (
-					<Link to="/addDataset">Add new dataset</Link>
-				)
-			}
-		</div>
+		<>
+			{!isLoading && (
+			<div className="d-flex justify-content-center mb-5 mt-5">
+				{location.pathname === '/addDataset' && (
+					<Link to="/">Back</Link>
+				)}
+				{
+					location.pathname !== '/addDataset' && (
+						<Link to="/addDataset">Add new dataset</Link>
+					)
+				}
+			</div>
+		)}
+		</>
 	)
 }
 
